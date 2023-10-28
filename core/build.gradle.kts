8cc995e3e6ff1f74ace8a6d82a4c7cb5ff6a3bb3
Blake Edwards <blake8090@gmail.com>
2023-10-28 18:19:53 -0400

    add gluqonhq maps

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,15 +1,30 @@
 
 plugins {
     kotlin("jvm") version "1.9.10"
+    id("org.openjfx.javafxplugin") version "0.1.0"
     application
 }
 
 repositories {
     mavenCentral()
+    maven {
+        setUrl("https://esri.jfrog.io/artifactory/arcgis")
+    }
 }
 
 dependencies {
     implementation("com.formdev:flatlaf:3.2.5")
+    implementation("org.openjfx:javafx-swing:22-ea+11")
+
+    implementation("com.gluonhq:maps:2.0.0-ea+6")
+
+    runtimeOnly("com.esri.arcgisruntime:arcgis-java-jnilibs:200.2.0")
+    runtimeOnly("com.esri.arcgisruntime:arcgis-java-resources:200.2.0")
+}
+
+javafx {
+    version = "17.0.8"
+    modules("javafx.controls", "javafx.graphics", "javafx.fxml", "javafx.web", "javafx.media")
 }
 
 application {
--- core/src/main/kotlin/bke/tracker/Main.kt
+++ core/src/main/kotlin/bke/tracker/Main.kt
@@ -1,10 +1,19 @@
 package bke.tracker
 
 import com.formdev.flatlaf.FlatDarkLaf
+import com.gluonhq.maps.MapLayer
+import com.gluonhq.maps.MapPoint
+import com.gluonhq.maps.MapView
+import javafx.application.Platform
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.embed.swing.JFXPanel
+import javafx.geometry.Point2D
+import javafx.scene.Node
+import javafx.scene.Scene
+import javafx.scene.layout.StackPane
 import java.awt.Dimension
 import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.SwingConstants
 import javax.swing.SwingUtilities
 import javax.swing.WindowConstants
 
@@ -15,12 +24,49 @@ fun main() {
         JFrame("Simple Flight Tracker").apply {
             defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
 
-            contentPane.add(JLabel("Hello World", SwingConstants.CENTER))
+            val fxPanel = JFXPanel()
+            add(fxPanel)
 
             preferredSize = Dimension(800, 600)
             pack()
             setLocationRelativeTo(null)
             isVisible = true
+
+            Platform.runLater {
+                initFX(fxPanel)
+            }
+        }
+    }
+}
+
+private fun initFX(panel: JFXPanel) {
+    val stackPane = StackPane()
+    val scene = Scene(stackPane)
+    panel.scene = scene
+
+    val mapView = MapView()
+    mapView.setPrefSize(800.0, 600.0)
+    mapView.zoom = 5.0
+    mapView.addLayer(CustomMapLayer())
+    stackPane.children.add(mapView)
+}
+
+class CustomMapLayer : MapLayer() {
+
+    private val points: ObservableList<Pair<MapPoint, Node>> = FXCollections.observableArrayList()
+
+    fun addPoint(p: MapPoint, icon: Node) {
+        points.add(p to icon)
+        children.add(icon)
+        markDirty()
+    }
+
+    override fun layoutLayer() {
+        for ((point, icon) in points) {
+            val mapPoint: Point2D = getMapPoint(point.latitude, point.longitude)
+            icon.isVisible = true
+            icon.translateX = mapPoint.getX()
+            icon.translateY = mapPoint.getY()
         }
     }
 }
