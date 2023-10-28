e99a4195f15389ed5d89d44df59b3b5c73e4903d
Blake Edwards <blake8090@gmail.com>
2023-10-28 15:31:14 -0400

    initial commit

---
+++ FlightTracker/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.FlightSimulator.SimConnect;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+const int WM_USER_SIMCONNECT = 0x0402;
+
+Console.WriteLine("Hello, World!");
+
+SimConnect? simConnect = null;
+
+try
+{
+    simConnect = new SimConnect("Managed Data Request", Process.GetCurrentProcess().MainWindowHandle, WM_USER_SIMCONNECT, null, 0);
+    Console.WriteLine("SimConnect has been started");
+}
+catch (COMException)
+{
+
+}
+
+if (simConnect != null)
+{
+    simConnect.Dispose();
+    Console.WriteLine("disposed");
+}
