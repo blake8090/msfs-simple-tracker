37aaaeb764bb71df9b3336213ce2abe0bf14be8a
Blake Edwards <blake8090@gmail.com>
2023-10-29 16:23:21 -0400

    remove unused imports

--- FlightTracker/src/SimConnectClient.cs
+++ FlightTracker/src/SimConnectClient.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.FlightSimulator.SimConnect;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FlightTracker
 {
--- FlightTracker/src/SimData.cs
+++ FlightTracker/src/SimData.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.FlightSimulator.SimConnect;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 
 enum DATA_REQUESTS
 {
