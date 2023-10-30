0117a40254026f571fe74771502971a5246d22f9
Blake Edwards <blake8090@gmail.com>
2023-10-29 20:54:55 -0400

    figured out a way to use ArcGIS with OpenStreetMaps without needing an API key

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,12 +11,18 @@ repositories {
     maven {
         setUrl("https://esri.jfrog.io/artifactory/arcgis")
     }
+    maven {
+        setUrl("https://olympus.esri.com/artifactory/arcgisruntime-repo")
+    }
 }
+
+val arcgisVersion by extra { "200.2.0" }
+
+val natives = configurations.create("natives")
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.3")
-
 
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-netty")
@@ -29,10 +35,11 @@ dependencies {
     implementation("com.formdev:flatlaf:3.2.5")
     implementation("org.openjfx:javafx-swing:22-ea+11")
 
-    implementation("com.gluonhq:maps:2.0.0-ea+6")
+    implementation("com.esri.arcgisruntime:arcgis-java:$arcgisVersion")
+    natives("com.esri.arcgisruntime:arcgis-java-jnilibs:$arcgisVersion")
+    natives("com.esri.arcgisruntime:arcgis-java-resources:$arcgisVersion")
 }
 
-// TODO: we still need this?
 javafx {
     version = "17.0.8"
     modules("javafx.controls", "javafx.graphics", "javafx.fxml", "javafx.web", "javafx.media")
@@ -40,4 +47,15 @@ javafx {
 
 application {
     mainClass.set("bke.tracker.MainKt")
+}
+
+task<Copy>("copyNatives") {
+    description = "Copies the arcgis native libraries into the project build directory for development."
+    group = "build"
+    val directory = "${System.getProperty("user.home")}/.arcgis/$arcgisVersion"
+    for (file in configurations.getByName("natives").asFileTree) {
+        println("unzipping file '${file.name}' into '$directory'")
+        from(zipTree(file))
+        into(directory)
+    }
 }
--- core/src/main/kotlin/bke/tracker/Gui.kt
+++ core/src/main/kotlin/bke/tracker/Gui.kt
@@ -1,19 +1,24 @@
 package bke.tracker
 
+import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.geometry.SpatialReferences
+import com.esri.arcgisruntime.layers.OpenStreetMapLayer
+import com.esri.arcgisruntime.loadable.LoadStatus
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.mapping.view.Graphic
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
+import com.esri.arcgisruntime.mapping.view.MapView
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.formdev.flatlaf.FlatDarkLaf
-import com.gluonhq.maps.MapLayer
-import com.gluonhq.maps.MapPoint
-import com.gluonhq.maps.MapView
 import javafx.application.Platform
-import javafx.collections.FXCollections
-import javafx.collections.ObservableList
 import javafx.embed.swing.JFXPanel
-import javafx.geometry.Point2D
-import javafx.scene.Node
 import javafx.scene.Scene
+import javafx.scene.control.Alert
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
-import javafx.scene.shape.Circle
 import mu.KotlinLogging
 import java.awt.Dimension
 import java.awt.event.WindowAdapter
@@ -25,7 +30,6 @@ import javax.swing.WindowConstants
 class Gui {
 
     private val log = KotlinLogging.logger {}
-    private val aircraftLayer = AircraftMapLayer()
 
     var onClose: () -> Unit = {}
 
@@ -65,40 +69,57 @@ class Gui {
             val scene = Scene(stackPane)
             panel.scene = scene
 
+            val layer = OpenStreetMapLayer()
+            // if the layer failed to load, display an alert
+            layer.addDoneLoadingListener {
+                if (layer.getLoadStatus() !== LoadStatus.LOADED) {
+                    Alert(
+                        Alert.AlertType.INFORMATION,
+                        "Open Street Map Layer failed to load"
+                    ).show()
+                }
+            }
+
+            // create a map and set the open street map layer as its basemap
+            val map = ArcGISMap()
+            map.basemap = Basemap(layer)
+
+            // create a map view and set the map to it
             val mapView = MapView()
-            mapView.zoom = 5.0
-            mapView.addLayer(aircraftLayer)
+            mapView.setMap(map)
+
             /*
+            SAMPLE DATA:
             Plane latitude: -37.0082111351168
             Plane longitude: 174.78156941940162
             Plane altitude: 24.680170927245964
-            */
-            val point = MapPoint(-37.0082111351168, 174.78156941940162)
-            aircraftLayer.addPoint(point, Circle(7.0, Color.RED))
+             */
+            val latitude = -37.0082111351168
+            val longitude = 174.78156941940162
 
+            // set a viewpoint on the map view
+            mapView.setViewpoint(Viewpoint(latitude, longitude, 577790.0))
+
+            val graphicsOverlay = GraphicsOverlay()
+            mapView.graphicsOverlays.add(graphicsOverlay)
+
+            // create a point geometry with a location and spatial reference
+            val point = Point(longitude, latitude, SpatialReferences.getWgs84())
+            // create an opaque orange point symbol with a opaque blue outline symbol
+            val simpleMarkerSymbol = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.RED, 10f)
+            val blueOutlineSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLUE, 2f)
+            simpleMarkerSymbol.outline = blueOutlineSymbol
+
+            // create a graphic with the point geometry and symbol
+            val pointGraphic = Graphic(point, simpleMarkerSymbol)
+
+            // add the point graphic to the graphics overlay
+            graphicsOverlay.graphics.add(pointGraphic)
+
+            // add the map view to stack pane
             stackPane.children.add(mapView)
         }
 
         return panel
-    }
-}
-
-class AircraftMapLayer : MapLayer() {
-
-    private val points: ObservableList<Pair<MapPoint, Node>> = FXCollections.observableArrayList()
-
-    fun addPoint(p: MapPoint, icon: Node) {
-        points.add(p to icon)
-        children.add(icon)
-        markDirty()
-    }
-
-    override fun layoutLayer() {
-        for ((point, icon) in points) {
-            val mapPoint: Point2D = getMapPoint(point.latitude, point.longitude)
-            icon.isVisible = true
-            icon.translateX = mapPoint.getX()
-            icon.translateY = mapPoint.getY()
-        }
     }
 }
