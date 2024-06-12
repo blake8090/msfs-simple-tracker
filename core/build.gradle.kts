e7b1aa7982c31a207a70244549ec6d1f1405522a
Blake Edwards <blake8090@gmail.com>
2024-06-12 19:04:44 -0400

    update arcgis and logback version

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
     }
 }
 
-val arcgisVersion by extra { "200.2.0" }
+val arcgisVersion by extra { "200.4.0" }
 
 val natives = configurations.create("natives")
 
@@ -28,7 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty")
     implementation("io.ktor:ktor-network")
 
-    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
     implementation("org.slf4j:slf4j-api:2.0.7")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
